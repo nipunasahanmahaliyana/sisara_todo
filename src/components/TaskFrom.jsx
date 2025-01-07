cf9131d369bb6b3bab873d2d73bbe72539627a07
N.S.D Mahaliyana <141614170+nipunasahanmahaliyana@users.noreply.github.com>
2025-01-07 14:00:59 +0530

    form input

--- src/components/TaskFrom.jsx
+++ src/components/TaskFrom.jsx
@@ -3,25 +3,38 @@ import './TaskForm.css';
 import Tag from './Tag';
 
 const TaskForm = () => {
-    const [task,setTask] = useState("");
+    const [taskData,seTaskData] = useState({
+        task:"",
+        status:"todo"
+    })
 
-    const handleTaskChange = (e) =>{
-        console.log(e.target.value);
-        setTask(e.target.value);
+    const handleChange = (e) =>{
+        const { name,value} = e.target;
+
+        seTaskData(prev => {
+            return {...prev,[name]:value}
+        })
     };
 
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(taskData);
+    }
+
     return (
-<header className='app_header'>
-    <form>
+    <header className='app_header'>
+    <form onSubmit={handleSubmit}>
             <input 
             type="text"
+            name="task"
              className="task_input" 
              placeholder='Enter your task'
-             onChange= {(e) => handleTaskChange(e)}>
+             onChange= {handleChange}
+             >
              </input>
 
             <div className='task_form_bottom_line'>
-                
+    
                 <div>
                 <Tag tagName="HTML" /> 
                 <Tag tagName="CSS" /> 
@@ -29,17 +42,24 @@ const TaskForm = () => {
                 </div>
 
                 <div>
-                <select className='task_status'>
+                <select 
+                name="status"
+                className='task_status'
+                onChange ={handleChange}
+                >
                     <option value="todo">To do</option>
-                    <option value="doing">To do</option>
-                    <option value="done">To do</option>
+                    <option value="doing">Doing</option>
+                    <option value="done">Done</option>
                 </select>
-                <button type="submit" className='task_submit'>+ Add Task</button>
+                <button 
+                type="submit"
+                 className='task_submit'
+                >+ Add Task</button>
                 </div>
 
             </div>
     </form>
-</header>
+    </header>
     );
 }
 
