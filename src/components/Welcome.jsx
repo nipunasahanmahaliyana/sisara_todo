79cc13ac19dec1bc5861ca5edd5a8b1cf45fa6d6
Nipuna Mahaliyana <141614170+nipunasahanmahaliyana@users.noreply.github.com>
2025-01-08 19:38:51 +0530

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -10,7 +10,7 @@ const WelcomeScreen = ({scrollToNextSection}) => {
       <div className="welcome-content">
         <img src={Logo} alt="sisara group" ></img>
         <h1 className="welcome-heading">Welcome to Sisara To Do App</h1>
-        <p className="welcome-subheading">We're glad to have you here! Explore the features and start your journey.</p>
+        <p className="welcome-subheading">We're glad to have you here! Explore the features.</p>
         <button className="start-button" onClick={scrollToNextSection}>
       Get Started
     </button>
